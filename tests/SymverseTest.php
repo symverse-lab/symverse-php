036d76a9ce1f02f0d5ab047ffe91a3f0cdc062c9
이신일 <itsinil@gmail.com>
2019-10-20 01:43:03 +0900

    wallet setting 작업

--- tests/SymverseTest.php
+++ tests/SymverseTest.php
@@ -4,6 +4,7 @@ use bb\Sha3\Sha3;
 use PHPUnit\Framework\TestCase;
 use Symverse\Symverse;
 
+//https://medium.com/coinmonks/generate-ethereum-wallet-key-pairs-using-php-ethereum-tutorial-c1cc75f0d64f
 final class SymverseTest extends TestCase
 {
     public function testSha3(): void {
